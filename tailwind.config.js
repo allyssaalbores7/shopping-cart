5cd771a9c52c01179e772a8efbd853aada364dda
allyviated <asalbores7@gmail.com>
2022-01-11 18:40:04 +0800

    Initial set-up

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    colors: {
+      accent: "#582be8",
+      red: "#d73a49",
+
+      light_border: "#e1e4e8",
+    },
+  },
+  extend: {
+    fontFamily: {
+      inter: ['"Inter"', "system-ui"],
+    },
+  },
+  plugins: [],
+};
